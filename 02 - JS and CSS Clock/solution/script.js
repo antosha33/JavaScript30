7ba2800494766db20421d0e26d1f84820977b498
Anton Yegorov <an_tosha33@mail.ru>
2019-09-04 22:03:32 +0300

    feat: finish the second task

---
+++ 02 - JS and CSS Clock/solution/script.js
@@ -0,0 +1,34 @@
+const hand = document.querySelectorAll('.hand');
+const arrowSecond = document.querySelector('.second-hand');
+const arrowMinute = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+const date = new Date();
+let seconds = date.getSeconds();
+let minutes = date.getMinutes();
+let hours = date.getHours();
+
+
+const setTime = () => {
+  const secondsDegrees = seconds/ 60* 360+90;
+  const minutesDegrees = (minutes/60*360) + (seconds*0.1)+90;
+  const hourDegrees =   (hours/12*360) + (minutes/60*30)+(seconds/3600*30) + 90;
+  arrowSecond.style.transform = `rotate(${secondsDegrees}deg)`;
+  arrowMinute.style.transform = `rotate(${minutesDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  seconds+=1;
+}
+
+const setMinuteHand = () => {
+  const minutedegrees = minutes/ 60* 360+90;
+  arrowMinute.style.transform = `rotate(${minutedegrees}deg)`;
+  if(minutes % 60 === 0)setHourHand();
+  minutes+=1
+}
+
+const setHourHand = () => {
+  const hoursdegrees = hours/ 12* 360+90;
+  hourHand.style.transform = `rotate(${hoursdegrees}deg)`;
+  hours+=1;
+}
+setInterval(setTime, 1000);
