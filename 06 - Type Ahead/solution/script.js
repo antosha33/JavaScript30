e337060b06cda5feea46fa712d3ff485b50d94f9
an_tosha33 <an_tosha33@mail.ru>
2019-09-06 14:50:38 +0300

    finish 7 task

---
+++ 06 - Type Ahead/solution/script.js
@@ -0,0 +1,35 @@
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const cities = [];
+
+(async function getCities(){
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  cities.push(...data);
+
+
+})();
+
+function findMatches(wordToMatch, cities){
+  return cities.filter((city) => {
+    const regex = new RegExp(wordToMatch, 'gi');
+    return city.city.match(regex) || city.state.match(regex);
+  })
+}
+const search = document.querySelector('input');
+const list = document.querySelector('ul');
+
+
+const searchHandler = (e) => {
+  const result = findMatches(e.target.value, cities);
+  const res = result.map((it) => {
+    return `<li>
+    <span class="name">${it.city}, ${it.state}</span>
+    <span class="population">${it.population}</span>
+    </li>`
+  });
+  list.innerHTML = res.join('');
+}
+
+search.addEventListener('keydown', searchHandler);
+
